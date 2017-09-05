7f42aad8d1328fdfd846494eecddc519768a7e59
cloudwerewolf <joshharc@gmail.com>
2017-09-05 07:39:26 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,17 +1,22 @@
-importScripts('/cache-polyfill.js');
-
-self.addEventListener('install', function(e) {
- e.waitUntil(
-   caches.open('airhorner').then(function(cache) {
-     return cache.addAll([
-       '/',
-       '/index.html',
-       '/main.css',
-       '/note.js'
-     ]);
-   })
- );
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(
+        [
+            '/',
+            '/index.html',
+            '/main.css',
+            '/note.js'
+        ]
+      );
+    })
+  );
 });
 self.addEventListener('fetch', function(event) {
     console.log(event.request.url);
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
 });
