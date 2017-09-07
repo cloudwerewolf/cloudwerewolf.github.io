7413e27bdb62894747a9315950baf224c1736629
cloudwerewolf <joshharc@gmail.com>
2017-09-07 06:49:43 +0200

    individual delete + sw

--- note.js
+++ note.js
@@ -1,23 +1,62 @@
 var notelist = [];
-
+function fade(element) {
+    var op = 1;
+    var timer = setInterval(function () {
+        if (op <= 0.1){
+            clearInterval(timer);
+            element.style.visibility = "hidden";
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op -= op * 0.1;
+    }, 30);
+}
+function delNote(id) {
+    id = id[0];
+    var note = document.getElementById(id);
+    var notediv = note.parentNode
+    console.log(notediv);
+    var div = document.getElementById("notepage");
+    div.removeChild(notediv);
+    notelist.splice(id, 1);
+}
+function showBtn(id) {
+    var btn = document.getElementById(id + "btn");
+    btn.style.visibility = "visible";
+    btn.style.opacity = 1;
+    setTimeout(function() {fade(btn);}, 1500);
+}
 function displayNotes() {
     var retnotelist = localStorage.getItem('Notelist');
     if (retnotelist !== null) {
         retnotelist = JSON.parse(retnotelist);
     }
     var div = document.getElementById("notepage");
-    for (var notes in retnotelist){
+    for (var notes in retnotelist) {
+        var notediv = document.createElement("div");
+        notediv.className = "noteholder";
+        div.appendChild(notediv);
+        var notespan = document.createElement("span");
+        notespan.className = "notebuttons";
+        notediv.appendChild(notespan);
         var text = document.createElement("textarea");
         text.name = "notes"
         text.cols = 45;
         text.rows = 5;
         text.className = "notelist";
-        this.id = notes;
+        text.onfocus = function() {showBtn(this.id);};
+        text.id = notes;
         if (retnotelist[notes].title != "") {
             text.value = retnotelist[notes].title.toUpperCase() + '\n' + retnotelist[notes].note;
         }
         else { text.value = retnotelist[notes].note;}
-        div.appendChild(text);
+        notediv.appendChild(text);
+        var xBtn = document.createElement("button");
+        xBtn.innerHTML = "X";
+        xBtn.id = notes + "btn";
+        xBtn.onfocus = function() {delNote(this.id);};
+        xBtn.style.visibility = "hidden";
+        notespan.appendChild(xBtn);
     }
 }
 function loadOld() {
@@ -60,12 +99,6 @@ function clearWall() {
 function clearNotes() {
     clearWall();
     displayNotes();
-}
-function deleteNotes() {
-    localStorage.removeItem('Notelist');
-    notelist = [];
-    localStorage.setItem('Notelist', JSON.stringify(notelist));
-    clearWall();
 }
 function regoSW() {
     if('serviceWorker' in navigator) {
---
+++ sw.js
@@ -0,0 +1,27 @@
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('note-cache').then(function(cache) {
+      return cache.addAll(
+        [
+            '/',
+            '/index.html',
+            '/main.css',
+            '/note.js',
+            '/manifest.json'
+        ]
+      );
+    })
+  );
+});
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.open('note-cache').then(function(cache) {
+      return cache.match(event.request).then(function (response) {
+        return response || fetch(event.request).then(function(response) {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
+});
